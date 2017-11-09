c194bfed7beedc3bc040fd2a9955f9c7c55b26c7
Leandro Novak <leandro.novak@outlook.com>
2017-11-09 18:59:39 +0000

    add some files

---
+++ connect-ssh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh aluno@192.168.56.101
---
+++ src/webserver.py
@@ -0,0 +1,10 @@
+# webserver.py
+# deve ser armazenado na pasta /usr/lib/cgi-bin/ da maquina virtual
+
+import cgi
+import cgitb
+
+cgitbm.enable(display=0, logdir="~/logs")
+
+
+
